6056ee0553b69175340eb10c3161bb6b0002692e
Agil Mammadov <mammadovagil@tutamail.com>
2026-03-24 21:45:21 +0400

    feat(nvim): modularize plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -18,20 +18,5 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-	{
-		'nvim-telescope/telescope.nvim', version = '*',
-		dependencies = { 'nvim-lua/plenary.nvim' }
-	}
-}
-local opts = {}
-
-require("lazy").setup(plugins, opts)
-
--- telescop
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+require("lazy").setup("plugins")
 
