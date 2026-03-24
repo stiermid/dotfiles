8721c38542900deeefbbffba488e6c962659ee9c
Agil Mammadov <mammadovagil@tutamail.com>
2026-03-24 22:24:17 +0400

    feat(nvim): much more modular
    seperate vim options

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,3 @@
-vim.cmd("set number")
-vim.cmd("set relativenumber")
-vim.g.mapleader = " "
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -18,5 +14,5 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+require("options")
 require("lazy").setup("plugins")
-
