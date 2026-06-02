fcc57bc7e11b9338ba6bc5331f669089ae12b964
Agil Mammadov <mammadovagil@proton.me>
2026-06-02 23:29:07 +0400

    nvim(lsp): add keymap for diagnostic

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -36,6 +36,7 @@ return {
 			-- keymaps
 			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+			vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, {})
 
 		end
 	}
